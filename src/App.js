6770d6f85c519a38ac6f3332af7a993ca65d96d8
Bennett Ballentine <bennett.ballentine@gmail.com>
2021-12-28 20:03:07 -0500

    refactored. X button on note now works

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import React, { useState } from "react";
 import "./App.css";
 import NoteBoard from "./components/NoteBoard.js";
-import MenuButton from "./components/MenuButton.js";
-import AddNoteButton from "./components/AddNoteButton";
-import Note from "./components/Note";
 
 function App() {
-	const [notes, setNotes] = useState([]);
-
-	const addNoteButtonHandler = () => {
-		let newNotesList = notes.concat(
-			<Note
-				key={notes.length}
-				noteIndex={notes.length}
-				allNotes={notes}
-			/>
-		);
-		setNotes(newNotesList);
-	};
-
-	return (
-		<div className="App">
-			<div className="HeaderBar">
-				<div className="filler"></div>
-				<h1 className="TitleText">Sticky Note Board</h1>
-				<AddNoteButton onClick={addNoteButtonHandler} />
-				<MenuButton />
-			</div>
-			<NoteBoard notes={notes} />
-		</div>
-	);
+  return (
+    <div className="App">
+      <NoteBoard />
+    </div>
+  );
 }
 
 export default App;
--- src/components/NoteBoard.js
+++ src/components/NoteBoard.js
@@ -1,11 +1,48 @@
+import React, { useState } from "react";
 import "./NoteBoard.css";
+import MenuButton from "./MenuButton.js";
+import AddNoteButton from "./AddNoteButton";
+import Note from "./Note";
 
-function NoteBoard(props) {
-	const notesToRender = props.notes.map((note) => {
-		return note;
-	});
+function NoteBoard() {
+  const [notes, setNotes] = useState([]);
+  const [nextNoteId, setNextNoteId] = useState(0);
 
-	return <div className="NoteBoard">{notesToRender}</div>;
+  const addNoteButtonHandler = () => {
+    let newNotesList = notes.concat({
+      id: nextNoteId,
+    });
+    setNotes(newNotesList);
+    setNextNoteId(nextNoteId + 1);
+  };
+
+  const exitButtonHandler = (id) => {
+    var remainingNotes = notes.filter((note) => {
+      return note.id !== id;
+    });
+
+    setNotes(remainingNotes);
+  };
+
+  return (
+    <div className="NoteBoardWrapper">
+      <div className="HeaderBar">
+        <div className="filler"></div>
+        <h1 className="TitleText">Sticky Note Board</h1>
+        <AddNoteButton onClick={addNoteButtonHandler} />
+        <MenuButton />
+      </div>
+      <div className="NoteBoard">
+        {notes.map((note) => (
+          <Note
+            key={note.id}
+            id={note.id}
+            exitButtonHandler={exitButtonHandler}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default NoteBoard;
