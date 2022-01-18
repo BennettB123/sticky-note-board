151098f8894e5510e1efdae00b6570f089a4d480
Bennett Ballentine <bennett.ballentine@gmail.com>
2022-01-18 16:06:54 -0500

    save notes in localStorage to make them persist

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,56 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Note.css";
 import Draggable from "react-draggable";
 import TripleDotIcon from "./assets/TripleDotIcon";
 import ExitIcon from "./assets/ExitIcon";
 
 function Note(props) {
-  // required for React not to throw errors about findDOMNode being deprecated
-  const nodeRef = React.useRef(null);
+	const [content, setContent] = useState(props.content);
 
-  // used to only drag if mouse is on the NoteHeader
-  const dragHandler = () => {
-    var noteHeader = [].slice.call(
-      document.getElementsByClassName("NoteHeader")
-    );
-    if (
-      !noteHeader.some((header) => {
-        return header.matches(":hover");
-      })
-    )
-      return false;
-  };
+	// required for React not to throw errors about findDOMNode being deprecated
+	const nodeRef = React.useRef(null);
 
-  return (
-    <Draggable
-      bounds="parent"
-      defaultPosition={{ x: 0, y: 0 }}
-      nodeRef={nodeRef}
-      onStart={dragHandler}
-    >
-      <div className="Note" ref={nodeRef}>
-        <div className="NoteHeader">
-          <div className="MenuButtonWrapper" title="open note menu">
-            <TripleDotIcon></TripleDotIcon>
-            {/* Potential Customization Options for Each Note:
-             * font family
-             * font size
-             * color
-             */}
-          </div>
+	// used to only drag if mouse is on the NoteHeader
+	const dragHandler = () => {
+		var noteHeader = [].slice.call(
+			document.getElementsByClassName("NoteHeader")
+		);
+		if (
+			!noteHeader.some((header) => {
+				return header.matches(":hover");
+			})
+		)
+			return false;
+	};
 
-          <div
-            className="ExitButtonWrapper"
-            onClick={() => props.exitButtonHandler(props.id)}
-            title="delete note"
-          >
-            <ExitIcon className="ExitButton" />
-          </div>
-        </div>
-        <textarea className="NoteTextField" placeholder="Type your note here" />
-      </div>
-    </Draggable>
-  );
+	const handleContentChange = (event) => {
+		setContent(event.target.value);
+	};
+
+	// Call noteUpdatedHandler when content is updated
+	useEffect(() => {
+		if (props.content !== content) {
+			props.noteUpdatedHandler(props.id, { content: content });
+		}
+	}, [content, props]);
+
+	return (
+		<Draggable
+			bounds="parent"
+			defaultPosition={{ x: 0, y: 0 }}
+			nodeRef={nodeRef}
+			onStart={dragHandler}
+		>
+			<div className="Note" ref={nodeRef}>
+				<div className="NoteHeader">
+					<div className="MenuButtonWrapper" title="open note menu">
+						<TripleDotIcon></TripleDotIcon>
+						{/* Potential Customization Options for Each Note:
+						 * font family
+						 * font size
+						 * color
+						 */}
+					</div>
+
+					<div
+						className="ExitButtonWrapper"
+						onClick={() => props.exitButtonHandler(props.id)}
+						title="delete note"
+					>
+						<ExitIcon className="ExitButton" />
+					</div>
+				</div>
+				<textarea
+					className="NoteTextField"
+					placeholder="Type your note here"
+					value={content}
+					onChange={handleContentChange}
+				/>
+			</div>
+		</Draggable>
+	);
 }
 
 export default Note;
--- src/components/NoteBoard.js
+++ src/components/NoteBoard.js
@@ -6,20 +6,61 @@ import Note from "./Note";
 
 function NoteBoard() {
 	const [notes, setNotes] = useState([]);
+	const [noteChanged, setNoteChanged] = useState(false);
 	const [nextNoteId, setNextNoteId] = useState(0);
-	const [wallColor, setWallColor] = useState("#ffffff");
+	const defaultNoteContent = "";
 
 	useEffect(() => {
+		// Get previous wall color from localStorage if exists
 		let previousWallColor = localStorage.getItem("wallColor");
 		if (previousWallColor) {
-			setWallColor(previousWallColor);
 			document.body.style.backgroundColor = previousWallColor;
 		}
-	}, [wallColor]);
+
+		// Get previous notes from localStorage if exists
+		let previousNotes = localStorage.getItem("notes");
+		if (previousNotes) {
+			retrieveNotesFromLocalStorage(previousNotes);
+		}
+	}, []);
+
+	// Update the notes in localStorage when a new is changed
+	useEffect(() => {
+		if (noteChanged) {
+			localStorage.setItem("notes", JSON.stringify(notes));
+			setNoteChanged(false);
+		}
+	}, [noteChanged, notes]);
+
+	const retrieveNotesFromLocalStorage = () => {
+		let json = localStorage.getItem("notes");
+		let retrievedNotes = JSON.parse(json);
+
+		// set new nextNoteId to avoid id collisions with previous notes.
+		if (retrievedNotes.length > 0) {
+			let newNoteId =
+				Math.max(...retrievedNotes.map((note) => note.id)) + 1;
+			setNextNoteId(newNoteId + 1);
+		}
+
+		setNotes(retrievedNotes);
+	};
+
+	const noteUpdatedHandler = (id, noteProperties) => {
+		let newNotes = notes;
+		newNotes.forEach((note) => {
+			if (note.id === id) {
+				note.content = noteProperties.content;
+			}
+		});
+		setNotes(newNotes);
+		setNoteChanged(true);
+	};
 
 	const addNoteButtonHandler = () => {
 		let newNotesList = notes.concat({
 			id: nextNoteId,
+			content: defaultNoteContent,
 		});
 		setNotes(newNotesList);
 		setNextNoteId(nextNoteId + 1);
@@ -30,11 +71,11 @@ function NoteBoard() {
 			return note.id !== id;
 		});
 		setNotes(remainingNotes);
+		setNoteChanged(true);
 	};
 
 	const wallColorCallback = (colorHexString) => {
 		localStorage.setItem("wallColor", colorHexString);
-		setWallColor(colorHexString);
 		document.body.style.backgroundColor = colorHexString;
 	};
 
@@ -51,7 +92,9 @@ function NoteBoard() {
 					<Note
 						key={note.id}
 						id={note.id}
+						content={note.content}
 						exitButtonHandler={exitButtonHandler}
+						noteUpdatedHandler={noteUpdatedHandler}
 					/>
 				))}
 			</div>
